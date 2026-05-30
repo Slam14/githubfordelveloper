fb2cea55daa79fb2f722ea4b3684e41560561606
Prasit <swong1971@gmail.com>
2026-05-30 22:54:03 +0700

    Create AddCard.js

---
+++ AddCard.js
@@ -0,0 +1,13 @@
+console.log("AddCard component loaded");
+
+function AddCard() {
+    return (
+        <div>
+            <h2>Add a New Card</h2>
+            <form>
+                {/* Card details form fields */}
+            </form>
+        </div>       
+    );
+}   
+
